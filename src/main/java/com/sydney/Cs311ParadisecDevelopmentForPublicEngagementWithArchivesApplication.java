f8d4d167b2dd7c7f4d28867e4fb7fe4292e87f65
yiweixu <511878701@qq.com>
2023-09-16 13:09:15 +1000

    init the backend environment

---
+++ src/main/java/com/sydney/Cs311ParadisecDevelopmentForPublicEngagementWithArchivesApplication.java
@@ -0,0 +1,13 @@
+package com.sydney;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cs311ParadisecDevelopmentForPublicEngagementWithArchivesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Cs311ParadisecDevelopmentForPublicEngagementWithArchivesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sydney/Cs311ParadisecDevelopmentForPublicEngagementWithArchivesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sydney;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cs311ParadisecDevelopmentForPublicEngagementWithArchivesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
